f6d8f3a6fa05c9ad5a10dc087e075b01de54f0ed
minho <kmn400@nate.com>
2021-11-17 22:03:06 +0900

    자르기 좋은 숫자

---
+++ lib/dart/D093.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  String inputs = stdin.readLineSync(encoding: utf8);
+  String a = inputs[0];
+  String b = inputs[1];
+  String c = inputs[2];
+  String d = inputs[3];
+
+  Cut cut = Cut();
+  print(cut.cal(a, b, c, d));
+}
+
+class Cut {
+  String result = '';
+
+  String cal(a, b, c, d) {
+    if (a == b && a == c && a == d && b == c && c == d) {
+      result = '$a$b$c$d';
+    } else {
+      result = 'NO';
+    }
+    return result;
+  }
+}
--- lib/lecture/cls1103.dart
+++ lib/lecture/cls1103.dart
@@ -4,9 +4,7 @@ void main() {
   Cleric cleric = Cleric(name: 'korea', hp: 100, mp: 50);
   cleric.mp = 30;
   print(cleric.pray(5));
-
 }
-
 
 class Cleric {
   //field, 전역, Global, property
@@ -17,7 +15,7 @@ class Cleric {
 
   // static 이라는 값이 들어간 함수는 시스템이 시작되면
   // 따로 저장공간에 관련 내용을 저장함.
-  // -> static을 클래스 내에 지정하는 것은 main 함수에 
+  // -> static을 클래스 내에 지정하는 것은 main 함수에
   static final int maxHp = 100;
   static final int maxMp = 50;
 
@@ -35,15 +33,12 @@ class Cleric {
     if (mp <= maxMp && mp > 0) {
       if (mp + random.nextInt(3) + sec >= maxMp) {
         return maxMp;
+      } else {
+        return mp + random.nextInt(3) + sec;
       }
-      else{
-      return mp + random.nextInt(3) + sec;
-    }
-    }
-    else if (mp > maxMp) {
+    } else if (mp > maxMp) {
       return maxMp;
-    }
-    else
+    } else
       return result;
   }
 }
