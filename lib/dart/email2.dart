05a73c5ed3ffd8a8c96126f1123d14dca64ac219
My_no <93500452+kmn400@users.noreply.github.com>
2021-11-21 17:27:22 +0900

    Delete email2.dart

--- lib/dart/email2.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  email('제목없음', address: 'kmn400@nate.com', text: '가나다라마바사');
-}
-
-void email(String title, {String address, String text}) {
-  print('$address에 아래의 메일을 송신한다.');
-  print('제목 : $title');
-  print('본문 : $text');
-}
