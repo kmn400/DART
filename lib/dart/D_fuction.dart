0e4b06abea25add96986b412bed8d5f8f446fd6c
minho <kmn400@nate.com>
2021-11-26 08:33:49 +0900

    fuction_함수형 프로그래밍

--- lib/dart/D_fuction.dart
+++ lib/dart/D_fuction.dart
@@ -30,7 +30,7 @@ void main() {
 
   // 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오
   Set<String> politan = {};
-  transactions.forEach((e) => politan.add(e.trader.city));
+  (transactions.forEach((e) => politan.add(e.trader.city)));
   print(politan);
 
   // 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
@@ -38,6 +38,8 @@ void main() {
       .forEach((e) => print(e.trader.name));
 
   // 4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
+
+
   // 5. 밀라노에 거래자가 있는가?
   // 6. 케임브리지에 거주하는 거래자의 모든 트랙잭션값을 출력하시오
   // 7. 전체 트랜잭션 중 최대값을 얼마인가?
