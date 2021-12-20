dc0fa72b15a233d7323ee67846ecdfeab20c5613
minho <kmn400@nate.com>
2021-12-20 23:15:50 +0900

    함수형프로그래밍_ANSWER

--- lib/dart/D_fuction.dart
+++ lib/dart/D_fuction.dart
@@ -27,25 +27,39 @@ void main() {
   (transactions.where((Transaction e) => e.year == 2011).toList()
         ..sort((a, b) => a.value.compareTo(b.value)))
       .forEach((e) => print(e.trader.name));
+  print('');
 
   // 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오
-  Set<String> politan = {};
-  (transactions.forEach((e) => politan.add(e.trader.city)));
-  print(politan);
-
+  (transactions.map((e) => e.trader.city).toSet().forEach((e) => print(e)));
+  print('');
   // 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
-  List<String> names =
-      transactions.where((e) => e.trader.city == "Cambridge").toSet().toList();
-  print(names);
+  (transactions.where((e) => e.trader.city == 'Cambridge').toList()
+        ..sort((a, b) => a.trader.name.compareTo(b.trader.name)))
+      .map((e) => e.trader.name)
+      .toSet()
+      .forEach((e) => print(e));
+  print('');
 
   // 4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
-  transactions
-      .where((e) => (e.trader.name) == true)
-      .toList()
+  (transactions..sort((a, b) => a.trader.name.compareTo(b.trader.name)))
+      .map((e) => e.trader.name)
+      .toSet()
       .forEach((e) => print(e));
-
+  print('');
   // 5. 밀라노에 거래자가 있는가?
+  print(transactions.any((e) => e.trader.city == 'Milan'));
+  print('');
+
   // 6. 케임브리지에 거주하는 거래자의 모든 트랙잭션값을 출력하시오
+  (transactions
+      .where((e) => e.trader.city == 'Cambridge')
+      .forEach((e) => print('${e.trader.name},${e.year},${e.value}')));
+  print('');
+
   // 7. 전체 트랜잭션 중 최대값을 얼마인가?
+  print((transactions..sort((a, b) => a.value.compareTo(b.value))).last.value);
+  print('');
+
   // 8. 전체 트랜잭션 중 최소값은 얼마인가?
+  print((transactions..sort((a, b) => -a.value.compareTo(b.value))).last.value);
 }
